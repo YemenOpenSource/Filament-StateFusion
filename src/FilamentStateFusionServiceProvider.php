8d537a25cdb62bd7aba7e2e98a25ddccfe62c089
A909M <119125167+A909M@users.noreply.github.com>
2025-02-01 22:44:18 +0000

    Fix styling

--- src/FilamentStateFusionServiceProvider.php
+++ src/FilamentStateFusionServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace A909M\FilamentStateFusion;
 
+use A909M\FilamentStateFusion\Commands\FilamentStateFusionCommand;
+use A909M\FilamentStateFusion\Testing\TestsFilamentStateFusion;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use A909M\FilamentStateFusion\Commands\FilamentStateFusionCommand;
-use A909M\FilamentStateFusion\Testing\TestsFilamentStateFusion;
 
 class FilamentStateFusionServiceProvider extends PackageServiceProvider
 {
